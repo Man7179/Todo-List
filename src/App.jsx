fe4eda31c80af0c38b53963a18a83aee7dd4ba42
Man7179 <123497531+Man7179@users.noreply.github.com>
2024-01-09 20:45:12 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/todos'
+function App() {
+  return (
+    <>
+  <h1>Learn about react Redux ToolKit</h1>  
+  <AddTodo />
+      <Todos />    
+    </>
+  )
+}
+
+export default App
